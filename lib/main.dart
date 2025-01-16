da2831f3e02f361462bd6b2520a9d5f3db03d46c
mariammasry <mariammasry04@gmail.com>
2025-01-16 22:16:34 +0200

    add live tracking page

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,30 @@ import 'package:amanak/theme/base_theme.dart';
 import 'package:amanak/home_screen.dart';
 import 'package:amanak/theme/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   BaseTheme lightTheme = LightTheme();
-   MyApp({super.key});
 
-  // This widget is the root of your application.
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: lightTheme.themeData,
-      debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName,
-      routes: {
-        HomeScreen.routeName :(context)=> HomeScreen(),
+    return ResponsiveSizer( // Wrap the app with ResponsiveSizer
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          theme: lightTheme.themeData,
+          debugShowCheckedModeBanner: false,
+          initialRoute: HomeScreen.routeName,
+          routes: {
+            HomeScreen.routeName: (context) => HomeScreen(),
+          },
+        );
       },
     );
   }
 }
-
-
-
-
